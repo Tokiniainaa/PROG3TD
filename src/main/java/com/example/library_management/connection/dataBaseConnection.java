d858c2135ffb1f203954e9bc26d47f7aeef042e3
tokiniaina <hei.tokiniaina@gmail.com>
2023-11-17 23:50:06 +0300

    create a database connection so that we can connect our application to the database and using environment variables for hidding de password,url and the user

---
+++ src/main/java/com/example/library_management/connection/dataBaseConnection.java
@@ -0,0 +1,24 @@
+package com.example.library_management.connection;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
+public class dataBaseConnection {
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                System.getenv("Url"),
+                System.getenv("User"),
+                System.getenv("Password")
+        );
+
+
+    }
+
+
+}
